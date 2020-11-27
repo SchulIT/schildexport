57ec969587c729805ac409efab07973cc2dd972a
Marcel Marnitz <marcelmarnitz@googlemail.com>
2020-11-27 21:32:36 +0100

    Fixes gender export

--- SchildExport/Entities/Geschlecht.cs
+++ SchildExport/Entities/Geschlecht.cs
@@ -4,18 +4,16 @@ namespace SchulIT.SchildExport.Entities
 {
     enum Geschlecht
     {
-        [MapValue('3')]
-        [MapValue(3)]
+        [MapValue(Value=3)]
         Maennlich,
 
-        [MapValue('4')]
-        [MapValue(4)]
+        [MapValue(Value = 4)]
         Weiblich,
 
-        [MapValue(' ')]
-        [MapValue('\u0004')]
-        [MapValue('\u0003')]
-        [MapValue(0)]
+        [MapValue(Value = ' ')]
+        [MapValue(Value = '\u0004')]
+        [MapValue(Value = '\u0003')]
+        [MapValue(Value = 0)]
         Unbekannt
     }
 }
